5afdff88fecc4ec66bfb796f067499d2eeacd6a7
Leko1705 <lennart1705@gmail.com>
2024-08-17 14:56:47 +0200

    - refactored execution invocation

--- src/main/java/com/test/language/run/TestRunConfiguration.java
+++ src/main/java/com/test/language/run/TestRunConfiguration.java
@@ -2,17 +2,45 @@ package com.test.language.run;
 
 import com.intellij.execution.*;
 import com.intellij.execution.configurations.*;
+import com.intellij.execution.filters.Filter;
+import com.intellij.execution.filters.HyperlinkInfo;
+import com.intellij.execution.impl.ConsoleViewImpl;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.ProgramRunner;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.execution.ui.ExecutionConsole;
 import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.test.exec.Main;
+import com.test.exec.tscript.runtime.core.FDEListener;
+import com.test.exec.tscript.runtime.core.TscriptVM;
+import com.test.exec.tscript.tscriptc.log.Logger;
+import com.test.exec.tscript.tscriptc.tools.Compiler;
+import com.test.exec.tscript.tscriptc.tools.CompilerProvider;
+import com.test.exec.tscript.tscriptc.util.Diagnostics;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Objects;
 
 public class TestRunConfiguration extends RunConfigurationBase<TestRunConfigurationOptions> {
 
@@ -48,16 +76,177 @@ public class TestRunConfiguration extends RunConfigurationBase<TestRunConfigurat
     public RunProfileState getState(@NotNull Executor executor,
                                     @NotNull ExecutionEnvironment environment) {
 
-        return new RunProfileState() {
+        DataContext context = environment.getDataContext();
+        if (context == null){
+            Messages.showErrorDialog("Can not find script file " + getScriptName(), "File Not Found");
+            return null;
+        }
+
+        VirtualFile file = context.getData(CommonDataKeys.VIRTUAL_FILE);
+        if (file == null) {
+            Messages.showErrorDialog("Can not find script file " + getScriptName(), "File Not Found");
+            return null;
+        }
+
+        String path = file.getPath();
+
+        return new CommandLineState(environment) {
+
             @Override
-            public @Nullable ExecutionResult execute(Executor executor, @NotNull ProgramRunner<?> runner) {
-                AnAction action = ActionManager.getInstance().getAction("com.tscript.RunAction");
-                AnActionEvent event = AnActionEvent.createFromAnAction(action, null, "MyCustomPlace", DataManager.getInstance().getDataContext());
-                action.actionPerformed(event);
-                return null;
+            protected @NotNull ProcessHandler startProcess() throws ExecutionException {
+
+                ConsoleView consoleView = createConsole(executor);
+                Objects.requireNonNull(consoleView);
+
+                PrintStream prevOut = System.out;
+                PrintStream prevErr = System.err;
+
+                TscriptProcessHandler handler = new TscriptProcessHandler(path, prevOut, prevErr);
+                consoleView.attachToProcess(handler);
+
+                System.setOut(new ConsoleOutputStream(prevOut, handler, ProcessOutputTypes.STDOUT));
+                System.setErr(new ConsoleOutputStream(prevErr, handler, ProcessOutputTypes.STDERR));
+
+                new Thread(handler).start();
+                return handler;
             }
         };
 
     }
+
+
+    private static class TscriptProcessHandler extends ProcessHandler implements Runnable {
+
+        private final String path;
+        private final PrintStream prevOut, prevErr;
+        private volatile boolean running = true;
+
+        public TscriptProcessHandler(String path, PrintStream prevOut, PrintStream prevErr) {
+            this.path = path;
+            this.prevOut = prevOut;
+            this.prevErr = prevErr;
+        }
+
+        @Override
+        protected void destroyProcessImpl() {
+            onTermination();
+            notifyProcessTerminated(0);
+        }
+
+        @Override
+        protected void detachProcessImpl() {
+            onTermination();
+            notifyProcessDetached();
+        }
+
+        @Override
+        public boolean detachIsDefault() {
+            return true;
+        }
+
+        @Override
+        public @Nullable OutputStream getProcessInput() {
+            return null;
+        }
+
+        @Override
+        public void run() {
+            String compiledPath = compile(path);
+            if (compiledPath == null){
+                notifyProcessTerminated(0);
+                return;
+            }
+
+            running = true;
+            int exitCode = exec(compiledPath);
+            System.out.println("\nExit Code: " + exitCode);
+            notifyProcessTerminated(exitCode);
+            onTermination();
+        }
+
+        private void onTermination(){
+            System.setOut(prevOut);
+            System.setErr(prevErr);
+            running = false;
+        }
+
+        private String compile(String path, String... args){
+            try (InputStream in = new FileInputStream(path)){
+                Compiler compiler = CompilerProvider.getDefaultTscriptCompiler();
+
+                OutputStream out = new FileOutputStream(path + "c");
+                compiler.run(in, out, new IDELogger(), args);
+                return path + "c";
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            catch (ProcessCanceledException e){
+                return null;
+            }
+        }
+
+        private int exec(String path){
+            try {
+                TscriptVM VM = TscriptVM.build(new File(path), System.out, System.err);
+                VM.attatchFDEListener(new InterruptListener());
+                return VM.execute();
+            }
+            catch (ProcessCanceledException e){
+                return -1;
+            }
+        }
+
+        private class InterruptListener implements FDEListener {
+            @Override
+            public void onAction() {
+                if (!running)
+                    throw new ProcessCanceledException();
+            }
+        }
+    }
+
+
+    private static class ConsoleOutputStream extends PrintStream {
+
+        private final ProcessHandler handler;
+        private final Key<?> outputType;
+
+        public ConsoleOutputStream(@NotNull OutputStream out, ProcessHandler handler, Key<?> outputType) {
+            super(out);
+            this.handler = handler;
+            this.outputType = outputType;
+        }
+
+        @Override
+        public void print(@Nullable String s) {
+            super.print(s);
+            handler.notifyTextAvailable(Objects.toString(s), outputType);
+        }
+
+        @Override
+        public void println(@Nullable String x) {
+            String s = x + '\n';
+            super.print(s);
+            handler.notifyTextAvailable(s, outputType);
+        }
+
+    }
+
+    private static class IDELogger implements Logger {
+
+        @Override
+        public void error(Diagnostics.Error error) {
+            System.err.println(error.toString());
+            throw new ProcessCanceledException();
+        }
+
+        @Override
+        public void warning(Diagnostics.Warning warning) {
+            // warnings already highlighted in IDE
+        }
+    }
+
+
 
 }
