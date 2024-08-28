f26c6d5ceea8b87fa68069273bfea7f1c49f6ca0
Leko1705 <lennart1705@gmail.com>
2024-08-28 13:44:02 +0200

    - fixed incorrect number parsing

--- src/main/java/com/tscript/ide/analysis/utils/TransformUtils.java
+++ src/main/java/com/tscript/ide/analysis/utils/TransformUtils.java
@@ -9,7 +9,7 @@ public class TransformUtils {
         int radix;
         if (s.charAt(1) == 'b')
             radix = 2;
-        else if (s.charAt(1) == '0')
+        else if (s.charAt(1) == 'o')
             radix = 8;
         else if (s.charAt(1) == 'x')
             radix = 16;
